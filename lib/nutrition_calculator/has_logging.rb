9ac22e8ec89aa47a2c3b52fe188cd1f774951c75
John Wilger <johnwilger@gmail.com>
2015-02-09 15:24:13 -0800

    Adds docs to HasLogging

--- lib/nutrition_calculator/has_logging.rb
+++ lib/nutrition_calculator/has_logging.rb
@@ -1,26 +1,39 @@
+require 'logger'
+
 module NutritionCalculator
+  # Provides a few logging utilities to the including class/module
   module HasLogging
     module NullLogger
       extend self
 
-      def noop(*args);end
+      def noop(*args); args; end
       alias_method :error, :noop
       alias_method :warn, :noop
       alias_method :info, :noop
       alias_method :debug, :noop
     end
+    private_constant :NullLogger
 
     attr_writer :logger
 
+    # The logger to which the including class/module should write
+    #
+    # Defaults to a NullLogger, which simply discards all logging attempts as
+    # noops.
+    #
+    # @return [Logger] an object adhering to the Logger interface
     def logger
       @logger ||= NullLogger
     end
 
-    private
+    protected
 
+    # Executes the block and both logs and returns the result
+    #
+    # @return [block.call] The result of calling the block provided
     def debug_value(name, &block)
-      block.call.tap do |v|
-        logger.debug("#{name}: #{v}")
+      block.call.tap do |value|
+        logger.debug("#{name}: #{value}")
       end
     end
   end
--- nutrition_calculator.gemspec
+++ nutrition_calculator.gemspec
@@ -27,4 +27,5 @@ Gem::Specification.new do |spec|
   spec.add_development_dependency "rspec", "~> 3.2.0"
   spec.add_development_dependency "cucumber", "~> 1.3.0"
   spec.add_development_dependency "reek"
+  spec.add_development_dependency "yard"
 end
